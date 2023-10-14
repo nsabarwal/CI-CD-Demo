92144ce50ea22e30d098b77de7e03da5d161987e
nsabarwal <136924417+nsabarwal@users.noreply.github.com>
2023-10-14 23:46:28 +0530

    Update ViewController.swift

--- CI-CD_Demo/ViewController.swift
+++ CI-CD_Demo/ViewController.swift
@@ -11,12 +11,14 @@ class ViewController: UIViewController {
 
     @IBOutlet private weak var nameTextField: UITextField!
     @IBOutlet private weak var greetingLabel: UILabel!
-    
+
+    //MARK: - Life cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
+    //MARK: - Button action -
     @IBAction private func actionHello(_ sender: UIButton) {
         greetingLabel.text = "Hello \(nameTextField.text ?? "")\n How are you ?"
     }
